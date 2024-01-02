70c480b323e07fc6100d6d9f62322f138ee7fd41
HasanHelvali <hasan.helvali.git@gmail.com>
2024-01-02 21:38:35 +0300

    end

--- Infrastructure/ECommerceAPI.Persistance/Configuration.cs
+++ Infrastructure/ECommerceAPI.Persistance/Configuration.cs
@@ -14,8 +14,19 @@ namespace ECommerceAPI.Persistance
             get 
             {
                 ConfigurationManager configurationManager = new();//microsoft.extension.configuration kutuphanesi gerekir.
-                configurationManager.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../../Presentation/ECommerceAPI.API"));//Microsoft.Extensions.Configuration.json kutuphanesi gerekir.
-                configurationManager.AddJsonFile("appsettings.json");//Microsoft.Extensions.Configuration.json
+                try
+                {
+                    /*Development ortamınd aburadaki dizin okuması basarılı olur. Deploy edilince uygulama tek bir yapı altında toplanır ve buradaki dosyalar
+                    okunamaz. Bu durumda da catch e duseriz. Deploy icin okumayı orada yapalım.*/
+                    configurationManager.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../../Presentation/ECommerceAPI.API"));//Microsoft.Extensions.Configuration.json kutuphanesi gerekir.
+                    configurationManager.AddJsonFile("appsettings.json");//Microsoft.Extensions.Configuration.json
+                }
+                catch (Exception)
+                {
+                    //Deploy ortamında ise okuma buradan yapılabilir.
+
+                    configurationManager.AddJsonFile("appsettings.Production.json");
+                }
                 return configurationManager.GetConnectionString("SqlServerConnections");
             }
         }
